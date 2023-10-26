ec63afa6290a50ee59ad642270d166661406f2a4
DongVuong <donguet.vnu@gmail.com>
2023-10-26 11:39:14 +0700

    Nop bai tap buoi 39

--- DAY39/js/regex.js
+++ DAY39/js/regex.js
@@ -1,6 +1,8 @@
-export const youtube =
-  /^((https|http)\:\/\/)*(www\.)*(youtube\.com|youtu\.be)\/(watch\?v=)*(.+)$/;
+// export const youtube =
+//   /^((https|http)\:\/\/)*(www\.)*(youtube\.com|youtu\.be)\/(watch\?v=)*(.+)$/;
 // export const youtubeNonHttps = /^((www\.)*(youtube\.com|youtu\.be)(\/.*)*)$/;
+export const youtube =
+  /(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=))([\w\-]{10,12})\b/;
 export const telephone = /^((?:0|\+84)\d{9})$/;
 export const telephoneNoPlus = /^((84)\d{9})$/;
 export const email = /^(([a-z0-9-_\.]{3,})@([a-z-_0-9\.]+\.[a-z]{2,}))$/;
