70346e151bc75dbbf0b6a955eb87275f242d7d5d
DongVuong <donguet.vnu@gmail.com>
2023-10-29 16:57:52 +0700

    Nop bai tap buoi 40

--- DAY40/Project01-vite-js/src/Utils/Router.js
+++ DAY40/Project01-vite-js/src/Utils/Router.js
@@ -1,6 +1,9 @@
 import Navigo from "navigo";
 const rootElm = document.querySelector("#app");
-const Base = new Navigo("/");
+// const Base = new Navigo("/");
+const Base = new Navigo(
+  "https://github.com/DongVuong/f8-fullstack-k3/tree/main/DAY40/Project01-vite-js"
+);
 window.navigate = (data) => Base.navigate(data);
 
 export const router = (pathList = [], DefaultLayout) => {
