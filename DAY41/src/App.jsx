ffd6151e5452acdb233e543a759c2a2ada892e90
DongVuong <donguet.vnu@gmail.com>
2023-11-06 01:10:10 +0700

    Nop bai tap buoi 41

--- DAY41/src/App.jsx
+++ DAY41/src/App.jsx
@@ -4,12 +4,29 @@ import { emailRegex } from "./helper/matchEmail";
 import { client } from "./api/client";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import DoubleBubble from "./helper/DoubleBubble.jsx";
 
 export default function App() {
-  const [apiKey, setApiKey] = useState(localStorage.getItem("apiKey"));
+  const [apiKey, setApiKey] = useState(sessionStorage.getItem("apiKey"));
+  const [isLoading, setIsLoading] = useState(false);
+  const handleLoading = (boolean) => {
+    setIsLoading(boolean);
+  };
+  const addLoading = (btn) => {
+    if (btn) {
+      btn.disabled = true;
+    }
+    handleLoading(true);
+  };
+  const removeLoading = (btn) => {
+    if (btn) {
+      btn.disabled = false;
+    }
+    handleLoading(false);
+  };
   useEffect(() => {
     if (apiKey) {
-      let name = localStorage.getItem("email");
+      let name = sessionStorage.getItem("email");
       name = name.split("@")[0];
       window.alert(`Chao mung ${name} quay tro lai`);
     } else if (!apiKey) {
@@ -29,11 +46,23 @@ export default function App() {
     const { data, response } = await client.get(`/api-key?email=${email}`);
     if (response.ok) {
       const apiKey = data.data.apiKey;
-      localStorage.setItem("apiKey", apiKey);
-      localStorage.setItem("email", email);
+      sessionStorage.setItem("apiKey", apiKey);
+      sessionStorage.setItem("email", email);
       setApiKey(apiKey);
     }
   };
 
-  return <GetTodo apiKey={apiKey} />;
+  return (
+    <>
+      {isLoading && <DoubleBubble />}
+      {
+        <GetTodo
+          apiKey={apiKey}
+          handleLoading={handleLoading}
+          addLoading={addLoading}
+          removeLoading={removeLoading}
+        />
+      }
+    </>
+  );
 }
--- DAY41/src/component/GetTodo.jsx
+++ DAY41/src/component/GetTodo.jsx
@@ -1,19 +1,154 @@
 import React, { useEffect, useState } from "react";
 import { client } from "../api/client";
+import { HtmlScript } from "../helper/StripHtml";
 
-export default function GetTodo({ apiKey }) {
+export default function GetTodo({
+  apiKey,
+  handleLoading,
+  addLoading,
+  removeLoading,
+}) {
+  function handleDelete(id) {
+    if (window.confirm("Bạn chắc chắn muốn xoá?")) {
+      addLoading();
+      client.setApiKey(apiKey);
+      client.delete(`/todos/${id}`).then(({ response, data }) => {
+        if (response.ok) {
+          removeLoading();
+          window.alert("Thao tác thành công");
+          client.get("/todos").then(({ response, data }) => {
+            setTodoList(data.data.listTodo);
+          });
+        } else {
+          removeLoading();
+          window.alert("Thao tác thất bại");
+        }
+      });
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (status) {
+      let todo = e.target.todo.value;
+      if (todo) {
+        addLoading();
+        e.target.todo.value = "";
+        client.setApiKey(apiKey);
+        client.post("/todos", { todo }).then(({ response, data }) => {
+          if (response.ok) {
+            removeLoading();
+            window.alert("Thao tác thành công");
+            client.get("/todos").then(({ response, data }) => {
+              setTodoList(data.data.listTodo);
+            });
+          } else {
+            removeLoading();
+            window.alert("Thao tác thất bại");
+          }
+        });
+      } else {
+        window.alert("Không được để trống công việc");
+      }
+    } else {
+      window.alert("Vui lòng đăng nhập để sử dụng dịch vụ");
+    }
+  }
+
   const [todoList, setTodoList] = useState([]);
+  let editDataInfo = {};
+  const [editData, setEditData] = useState({
+    editingId: null,
+    inputText: "",
+  });
+  let status = null;
+  status = apiKey;
   useEffect(() => {
     client.setApiKey(apiKey);
-    client.get("/todos").then(({ response, data }) => {
-      setTodoList(data.data.listTodo);
+    if (status) {
+      addLoading();
+      client.get("/todos").then(({ response, data }) => {
+        if (response.ok) {
+          setTodoList(data.data.listTodo);
+        } else {
+          addLoading();
+          window.alert("Mất kết nối, vui lòng đăng nhập lại");
+          sessionStorage.clear();
+          window.location.reload();
+        }
+      });
+      removeLoading();
+    }
+  }, [status]);
+  function handleEdit(id, todo, isCompleted) {
+    editDataInfo = {
+      id,
+      todo,
+      isCompleted,
+    };
+    setEditData({
+      editingId: id,
+      inputText: todo,
     });
-  }, []);
+  }
+  const handleUpdateClick = (id, e) => {
+    const body = todoList.find((item) => {
+      return id === item._id;
+    });
+    addLoading();
+    client.setApiKey(apiKey);
+    client
+      .patch(`/todos/${id}`, {
+        todo: editData.inputText,
+        isCompleted: body.isCompleted,
+      })
+      .then(({ response, data }) => {
+        removeLoading();
+        if (response.status === 401) {
+          this.reloadPage();
+        } else if (response.ok) {
+          setTodoList(
+            todoList.map((item) =>
+              item._id === id ? { ...item, todo: editData.inputText } : item
+            )
+          );
+          setEditData({
+            editingId: null,
+          });
+          window.alert(`${data.message}`);
+        } else {
+          handleCannelClick();
+          window.alert("Đã xảy ra lỗi, hãy thử lại!");
+        }
+        removeLoading();
+      });
+  };
+  const handleChecked = (id) => {
+    setTodoList(
+      todoList.map((item) =>
+        item._id === id ? { ...item, isCompleted: !item.isCompleted } : item
+      )
+    );
+  };
+  const handleCannelClick = () => {
+    const { id, isCompleted: oldCompleted } = editDataInfo;
+    setTodoList(
+      todoList.map((item) =>
+        item._id === id ? { ...item, isCompleted: oldCompleted } : item
+      )
+    );
+    setEditData({
+      editingId: null,
+    });
+  };
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setEditData({ ...editData, inputText: value });
+  };
   return (
     <main className="__className_6a793a flex items-center justify-center p-8">
       <div className="container bg-slate-700 p-4 flex flex-col justify-center items-center">
         <h1 className="font-bold text-white">Wellcome to App!</h1>
-        <form action="" className="w-full max-w-sm">
+        <form action="" className="w-full max-w-sm" onSubmit={handleSubmit}>
           <div className="flex items-center border-b border-teal-500 py-2 relative">
             <input
               type="text"
@@ -46,34 +181,89 @@ export default function GetTodo({ apiKey }) {
               />
             </li>
           ) : (
-            todoList.map(({ todo, _id: id }) => (
-              <li
-                key={id}
-                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2"
-              >
-                <input
-                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  "
-                  value={todo}
-                  readOnly
-                />
-                <div className="flex items-center justify-between mt-4">
-                  <div className="flex items-center">
-                    <button
-                      type="button"
-                      className="bg-teal-500 hover:bg-teal-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
-                    >
-                      Sửa
-                    </button>
-                    <button
-                      type="button"
-                      className="bg-red-500 hover:bg-red-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
-                    >
-                      Xóa
-                    </button>
+            todoList.map(({ todo, _id: id, isCompleted }) =>
+              editData.editingId === id ? (
+                <li
+                  key={id}
+                  className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2"
+                >
+                  <input
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  "
+                    value={HtmlScript(editData.inputText)}
+                    onChange={(e) => handleInputChange(e)}
+                  />
+                  <div className="flex items-center justify-between mt-4">
+                    <div className="flex items-center">
+                      <label htmlFor={id} className="mr-2">
+                        Completed
+                      </label>
+                      <input
+                        type="checkbox"
+                        id={id}
+                        checked={isCompleted}
+                        onChange={() => handleChecked(id)}
+                        className="form-checkbox h-5 w-5 text-gray-600 "
+                      />
+                    </div>
+                    <div className="flex items-center">
+                      <button
+                        type="button"
+                        onClick={handleCannelClick}
+                        className="bg-orange-500 hover:bg-orange-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+                      >
+                        Thoát
+                      </button>
+                      <button
+                        type="button"
+                        onClick={(e) => handleUpdateClick(id, e)}
+                        className="bg-teal-500 hover:bg-teal-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+                      >
+                        Update
+                      </button>
+                      <button
+                        type="button"
+                        onClick={(e) => handleDelete(id, e)}
+                        className="bg-red-500 hover:bg-red-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
+                      >
+                        Xóa
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))
+                </li>
+              ) : (
+                <li
+                  key={id}
+                  className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2"
+                >
+                  <input
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  "
+                    value={todo}
+                    style={
+                      isCompleted ? { textDecoration: "line-through" } : {}
+                    }
+                    readOnly
+                  />
+                  <div className="flex items-center justify-between mt-4">
+                    <div className="flex items-center">
+                      <button
+                        onClick={() => handleEdit(id, todo, isCompleted)}
+                        type="button"
+                        className="bg-teal-500 hover:bg-teal-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+                      >
+                        Sửa
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => handleDelete(id)}
+                        className="bg-red-500 hover:bg-red-700 text-white  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline "
+                      >
+                        Xóa
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              )
+            )
           )}
         </ul>
       </div>
